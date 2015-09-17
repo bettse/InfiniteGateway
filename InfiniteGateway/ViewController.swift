c63ebdf77839bc0733eddc6279fb95cb78f22c52
Eric Betts <bettse@fastmail.fm>
2015-09-17 16:30:20 -0700

    Initial Commit

---
+++ InfiniteGateway/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InfiniteGateway
+//
+//  Created by Eric Betts on 9/17/15.
+//  Copyright © 2015 Eric Betts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
