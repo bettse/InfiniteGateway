c199a29f913ef004cc029351e6620d117635dc65
Eric Betts <bettse@fastmail.fm>
2015-09-20 13:56:07 -0700

    Experimental Block class

---
+++ InfiniteGateway/Block.swift
@@ -0,0 +1,18 @@
+//
+//  Block.swift
+//  InfiniteGateway
+//
+//  Created by Eric Betts on 9/20/15.
+//  Copyright © 2015 Eric Betts. All rights reserved.
+//
+
+import Foundation
+
+class Block : NSMutableData {
+    //Get checksum
+    //update checksum
+    //validate checksum
+    //encrypt
+    //decrypt
+    
+}
